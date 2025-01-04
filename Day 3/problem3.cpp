d831726eea149084fa9b832ce4a08c23128ff2d5
shubham <shubhamxpawar27@gmail.com>
2025-01-04 22:12:11 +0530

    Day3

---
+++ Day 3/problem3.cpp
@@ -0,0 +1,14 @@
+//codeforces - (A) 1 to N
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int N, i = 1;
+    cin >> N;
+
+    while(i <= N) cout << i++ << endl;
+
+    return 0;
+}
